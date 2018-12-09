989e8d073ed18f271430d4a4b081734a0dcc4bb9
kdhageman <kasparhageman@gmail.com>
2018-12-09 16:44:38 +0100

    Randomize initial direction of ball

--- Assets/Scripts/MoveBall.cs
+++ Assets/Scripts/MoveBall.cs
@@ -8,16 +8,16 @@ public class MoveBall : MonoBehaviour
 	public float minVelocity = 10;
 	public float maxVelocity = 30;
 	public Canvas canvas;
-	
+
 	private Rigidbody2D _rb;
 	private Scoreboard _sb;
-	
+
 	// Use this for initialization
-	void Start ()
+	void Start()
 	{
 		this._rb = gameObject.GetComponent<Rigidbody2D>();
 		this._sb = canvas.GetComponent<Scoreboard>();
-		this.RandomDirection();
+		this.Reset();
 	}
 
 	private void NormalizeVelocity()
@@ -26,14 +26,16 @@ public class MoveBall : MonoBehaviour
 		if (curVelocity > this.maxVelocity)
 		{
 			this._rb.velocity = this._rb.velocity.normalized * maxVelocity;
-		} else if (curVelocity < this.minVelocity)
+		}
+		else if (curVelocity < this.minVelocity)
 		{
 			this._rb.velocity = this._rb.velocity.normalized * minVelocity;
 		}
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
 		this.NormalizeVelocity();
 	}
 
@@ -48,17 +50,22 @@ public class MoveBall : MonoBehaviour
 				this._sb.Left();
 				break;
 		}
-		this.ResetPosition();
-		this.RandomDirection();
+
+		this.Reset();
 	}
 
-	void ResetPosition()
+	void Reset()
 	{
 		this._rb.position = Vector2.zero;
-	}
+		this._rb.velocity = Vector2.zero;
 
-	void RandomDirection()
-	{
-		this._rb.velocity = Vector2.left * this.velocity;
+		float angle = Random.Range(80, 100);
+		if (Random.Range(0, 2) >= 1)
+		{
+			angle += 180;
+		}
+		this._rb.MoveRotation(angle);
+		this._rb.AddRelativeForce(Vector2.up * this.velocity);
 	}
 }
+
